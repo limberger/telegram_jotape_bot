ec81ea550ddfd20259c839357998837ec3306561
F4955639 Joao Paulo Limberger <f4955639@bb.com.br>
2018-09-04 19:17:56 -0300

    adjust

--- bothandler.py
+++ bothandler.py
@@ -8,10 +8,13 @@ class BotHandler:
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
     def get_updates(self, offset=None, timeout=30):
+        print("get_updates() timeout= %s ",timeout)
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
         result_json = resp.json()['result']
+        print("Result do get_updates ...")
+        print(result_json)
         return result_json
 
     def send_message(self, chat_id, text):
